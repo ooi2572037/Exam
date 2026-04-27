64c939dd8210449cea74bd0f7afcd2f16aae9328
yuuenjoy313 <yuu.enjoy190313@gmail.com>
2026-04-27 14:33:24 +0900

    Add files via upload

---
+++ src/main/java/bean/Subject.java
@@ -0,0 +1,33 @@
+package bean;
+
+public class Subject {
+	private String subjectCd;
+	
+	private String subjectName;
+	
+	private School school;
+
+	public String getSubjectCd() {
+		return subjectCd;
+	}
+
+	public void setSubjectCd(String subjectCd) {
+		this.subjectCd = subjectCd;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	public School getSchool() {
+		return school;
+	}
+
+	public void setSchool(School school) {
+		this.school = school;
+	}
+}
